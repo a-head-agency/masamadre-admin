49777db997838a689b624a0b9d1b07aba32a76f2
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-06-16 12:57:52 +0300

    remove promo import

--- src/features/settings/index.ts
+++ src/features/settings/index.ts
@@ -1,4 +1,5 @@
 export { default as DocumentsForm } from './DocumentsForm.vue'
-export { default as CommonSettingsForm } from './CommonSettingsForm.vue'
-export { default as BroadcastNotification } from './BroadcastNotification.vue'
-export { default as Promo } from './Promo.vue'
+export { default as     CommonSettingsForm } from './CommonSettingsForm.vue'
+export { default as BroadcastNotification 
+  
+} from './BroadcastNotification.vue'
