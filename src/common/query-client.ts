8fb6f2a18eb3580f7b333035844b97116f442120
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-06-19 18:03:54 +0300

    add error logging

--- src/common/query-client.ts
+++ src/common/query-client.ts
@@ -1,9 +1,20 @@
 import { QueryClient } from '@tanstack/vue-query'
+import { ZodError } from 'zod'
 
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            staleTime: 60 * 1000
+            staleTime: 60 * 1000,
+            retry(failureCount, error) {
+                if (error instanceof ZodError) {
+                    console.log('ОШИБКА ВАЛИДАЦИИ', error.errors)
+                    return false
+                }
+                if (failureCount <= 2) {
+                    return true
+                }
+                return false
+            }
         }
     }
 })
